a40845d4f0a51b50b199c08341403c967b430ab0
Mac Rowe <m.rowe42@gmail.com>
2021-04-21 10:05:43 -0400

    commit tree 10de188034eb3f34dcdca846a55ae5b22d194ed3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,12 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    @student.toggle(:active).save
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
